96bec73dfb55974b7f7867c05f89e816bccc8af2
Bobby Alv <metzgr@gmail.com>
2025-03-05 08:43:28 -0600

    init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,24 @@
-import { Button } from "@workspace/ui/components/button"
+"use client"
+import Hero from "../components/hero";
+import { ConvexNudge } from '@convex-nudge/react';
 
-export default function Page() {
+export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-      </div>
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center p-20 gap-16 font-synthesis-none">
+      <main className="flex flex-col gap-8 row-start-2">
+        <Hero />
+      </main>
+      <footer className="grid-row-start-3 flex gap-6 bottom-10">
+        <ConvexNudge
+          variant="dark"
+          position="bottom"
+          animation="slide"
+          textSize="base"
+          logoSize={24}
+          referralCode="cvx-starter"
+          fixed
+        />
+      </footer>
     </div>
-  )
+  );
 }
---
+++ apps/web/components/hero.tsx
@@ -0,0 +1,44 @@
+"use client"
+import { TypewriterEffectSmooth } from "@workspace/ui/components/typewriter-effect"
+import { Button } from "@workspace/ui/components/button"
+import { GithubIcon } from "lucide-react"
+
+export default function Hero() {
+  const words = [
+    {
+      text: "Build",
+    },
+    {
+      text: "awesome",
+    },
+    {
+      text: "apps",
+    },
+    {
+      text: "with",
+    },
+    {
+      text: "this",
+    },
+    {
+      text: "Convex Starter.",
+      className: "text-pink-500 dark:text-pink-500",
+    },
+  ]
+  return (
+    <div className="flex flex-col items-center justify-center h-[40rem]  ">
+      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
+        The road to freedom starts from here
+      </p>
+      <TypewriterEffectSmooth words={words} />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+        <Button className="w-40 h-10 bg-black border dark:border-white border-transparent text-white text-sm">
+          <GithubIcon className="size-4" />
+          Github
+        </Button>
+        <Button className="w-40 h-10 bg-white text-black border border-black  text-sm">Signup</Button>
+      </div>
+    </div>
+  )
+}
+
